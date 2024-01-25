b456dc6d10af899601212075fccebe6df52a7011
Papaioannou Christoforos <xristofpap@gmail.com>
2024-01-25 15:40:28 +0200

    feat: uploaded file for the online metrics

---
+++ WP3/get_online_metrics_ex_1_pr_1.py
@@ -0,0 +1,17 @@
+
+def get_metrics(imu1,imu2,imu3,imu4):
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    c4 = 0
+    #for i in imu1:
+    #    c1= c1+1
+    #for i in imu2:
+    #    c2= c2+1
+    #for i in imu3:
+    #    c3= c3+1
+    #for i in imu4:
+    #    c4= c4+1
+    #print(len(imu1))
+    print("!!! ", len(imu1))
+    return(imu1)
