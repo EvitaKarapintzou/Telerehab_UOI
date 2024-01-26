5aefa49e2cc4e69cdb3de0c29c5ac4aa8313082b
Papaioannou Christoforos <xristofpap@gmail.com>
2024-01-26 14:41:11 +0200

    fix: remove comments

--- WP3/imu_sender.py
+++ WP3/imu_sender.py
@@ -12,8 +12,6 @@ broker = '192.168.0.231'
 port = 1883
 topic = "location/123"
 client_id = f'publish-{random.randint(0, 1000)}'
-# username = 'emqx'
-# password = 'public'
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
@@ -23,7 +21,6 @@ def connect_mqtt():
             print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
@@ -33,13 +30,8 @@ def format_message(name, mac,row):
     m = ""
     for i in range(len(splitRow)):
         m = m + " " + str(splitRow[i])
-    #print("WWWW " + str(m))
-    
     message = name + " " + mac + m
     return message
-
-#MetaWear61 E15561CB9161 1706108668349 2024-01-24T17:04:28.349 58.4 0.9966065 -0.054574832 0.06161933 -1.982326E-5
-    
 
 def publish(client):
     msg_count = 1
@@ -58,24 +50,21 @@ def publish(client):
     imu4Data = []
 
     for row in imu1Reader:
-        #print(', '.join(row))
         imu1Data.append(format_message("MetaWear61", "E15561CB9161", ', '.join(row)))
         
     for row in imu2Reader:
-        #print(', '.join(row))
         imu2Data.append(format_message("MetaWear94", "E25AD03D0194", ', '.join(row)))
 
     for row in imu3Reader:
-        #print(', '.join(row))
         imu3Data.append(format_message("MetaWearBD", "C8925E7DC6BD", ', '.join(row)))
 
     for row in imu4Reader:
-        #print(', '.join(row))
         imu4Data.append(format_message("MetaWearE7", "FEAC84C53DE7", ', '.join(row)))
 
     sendData = []
     
     for i in range(min(len(imu1Data),len(imu2Data),len(imu3Data), len(imu4Data))):
+
         sendData.append(imu1Data[i])
         sendData.append(imu2Data[i])
         sendData.append(imu3Data[i])
@@ -84,11 +73,10 @@ def publish(client):
         print(sendData)
         if(len(sendData) >=100):
             sendDataString = ""
-            
-            result = client.publish(topic, str(sendData))
-            sendData = []
-            #time.sleep(0.3)
 
+            sendDataString = ", ".join(["'" + item + "'" for item in sendData])
+            result = client.publish(topic, str(sendDataString))
+            sendData = []
 
 def run():
     client = connect_mqtt()
@@ -96,7 +84,6 @@ def run():
     publish(client)
     print("telos")
     client.loop_stop()
-
 
 if __name__ == '__main__':
     run()
--- WP3/mqtt_client.py
+++ WP3/mqtt_client.py
@@ -4,7 +4,6 @@ from multiprocessing import Process, Manager, Value
 import time
 from get_online_metrics_ex_1_pr_1 import get_metrics
 
-
 MQTT_BROKER_HOST = '192.168.0.231'
 MQTT_BROKER_PORT = 1883
 MQTT_KEEP_ALIVE_INTERVAL = 60
@@ -16,7 +15,6 @@ imu3Queue = mp.Queue();
 imu4Queue = mp.Queue();
 scheduleQueue = mp.Queue();
 
-
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("location/123")
@@ -27,13 +25,7 @@ def on_message(client, userdata, msg):
     #print("Message Recieved. ", msg.payload.decode(), " ", counter)
     queueData.put(msg.payload.decode())
 
-
 def get_data_tranch(q1,q2,q3,q4):
-
-    # tempQ1 = q1
-    # tempQ2 = q2
-    # tempQ3 = q3
-    # tempQ4 = q4
 
     imu1List = []
     imu2List = []
@@ -53,53 +45,21 @@ def get_data_tranch(q1,q2,q3,q4):
         imu4List.append(q4.get())
 
     get_metrics(imu1List,imu2List,imu3List,imu4List)
-    
-    #while not imu1Queue.empty():
-    #	imu1Queue.get()
-    #while not imu2Queue.empty():
-    #	imu2Queue.get()
-    #while not imu3Queue.empty():
-    #	imu3Queue.get()
-    #while not imu4Queue.empty():
-    #	imu4Queue.get()
 
 def scheduler(scheduleQueue):
-	#for i in range(80):
 	while(True):
 		time.sleep(0.5)
 		scheduleQueue.put("GO");
 	
-
 def receive_imu_data(q,scheduleQueue):
     while(True):
-
-		
-        #while not imu1Queue.empty():
-        #    imu1Queue.get()
-        #while not imu2Queue.empty():
-        #    imu2Queue.get()
-        #while not imu3Queue.empty():
-        #    imu3Queue.get()
-        #while not imu4Queue.empty():
-        #    imu4Queue.get()
             
         while(not q.empty()):
             A = q.get()
             parts = A.split(",")
             
-            # imu1Queue.put("1")
-            # imu2Queue.put("2")
-            # imu3Queue.put("3")
-            # imu4Queue.put("4")
-            
-            #while not q.empty():
-            #	q.get()
-            
-            #print(A)
-            
             for item in parts:
               name = item.split()
-             # print(name[1])
               if(name[1] == "E15561CB9161"):
                   imu1Queue.put(item)
               elif(name[1] == "E25AD03D0194"):
@@ -109,12 +69,8 @@ def receive_imu_data(q,scheduleQueue):
               elif(name[1] == "FEAC84C53DE7"):
                   imu4Queue.put(item)
 
-            
-                 
-            #print(imu1Queue.qsize(), " ", imu2Queue.qsize(), " ", imu3Queue.qsize(), " ", imu4Queue.qsize())
             if(not scheduleQueue.empty()):
                 get_data_tranch(imu1Queue,imu2Queue,imu3Queue,imu4Queue)
-                
                 scheduleQueue.get()
 
 mbB = mp.Process(target=receive_imu_data,args=(queueData,scheduleQueue,));
@@ -126,6 +82,5 @@ client = mqtt.Client()
 client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, MQTT_KEEP_ALIVE_INTERVAL)
 client.on_connect = on_connect
 client.on_message = on_message
-
 
 client.loop_forever()
