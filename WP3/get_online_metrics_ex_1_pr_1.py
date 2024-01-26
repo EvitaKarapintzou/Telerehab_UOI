bbfdf6bff018913d5804b197cb2660c5d6ac2956
Papaioannou Christoforos <xristofpap@gmail.com>
2024-01-26 14:40:19 +0200

    fix: print a row of imu4 data

--- WP3/get_online_metrics_ex_1_pr_1.py
+++ WP3/get_online_metrics_ex_1_pr_1.py
@@ -1,7 +1,8 @@
 
 def get_metrics(imu1,imu2,imu3,imu4):
-
     
-    print(" imu1 ", len(imu1), " imu2 ", len(imu2), " imu3 ", len(imu3), " imu4 ", len(imu4)) 
-    print(imu1)
-    return(imu1)
+    #print(" imu1 ", len(imu1), " imu2 ", len(imu2), " imu3 ", len(imu3), " imu4 ", len(imu4)) 
+    if(len(imu4) > 2):
+        print(imu4[1])
+
+    return(imu1)																																								
