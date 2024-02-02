9edbfdbf510b2b6c1e2dead1b98ee7170e545f0c
Papaioannou Christoforos <xristofpap@gmail.com>
2024-02-02 14:01:47 +0200

    feat: added the online metrics function

---
+++ WP3/imu_mqtt/get_online_metrics_ex_1_pr_1.py
@@ -0,0 +1,17 @@
+
+def get_metrics(imu1,imu2,imu3,imu4):
+    
+    #print(" imu1 ", len(imu1), " imu2 ", len(imu2), " imu3 ", len(imu3), " imu4 ", len(imu4)) 
+    #if(len(imu1) > 0):
+    #    print(len(imu1))
+    #for item in imu1:
+    #    print(item)
+    save_data(imu1,imu2,imu3,imu4)
+    return(imu1)	
+
+def save_data(imu1,imu2,imu3,imu4):
+    if(len(imu1) > 0):
+        print(len(imu1))
+
+
+																																	
