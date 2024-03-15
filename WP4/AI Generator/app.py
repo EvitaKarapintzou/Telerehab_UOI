2e900033b75a6dc6af7fd3d948366280684122b8
Papaioannou Christoforos <xristofpap@gmail.com>
2024-03-15 10:15:13 +0200

    fix: updated the output json

--- WP4/AI Generator/app.py
+++ WP4/AI Generator/app.py
@@ -1,8 +1,16 @@
 from crypt import methods
 from flask import Flask,jsonify,request
 from flask_swagger_ui import get_swaggerui_blueprint
+from werkzeug.utils import secure_filename
+import pandas as pd
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'xlsx'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 SWAGGER_URL="/swagger"
 API_URL="/static/swagger.json"
 
@@ -15,31 +23,31 @@ swagger_ui_blueprint = get_swaggerui_blueprint(
 )
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route("/ping")
 def home():
     return jsonify({
         "Message": "app up and running successfully"
     })
 
-@app.route("/optimal_intervation_plan_prediction",methods=["POST"])
+@app.route("/optimal_intervation_plan_prediction", methods=["POST"])
 def optimal_intervation_plan_prediction():
     data = request.get_json()
-    print(data)
-    input = data.get("patient_onboarding_data(baseline)", "string")
-
-    message = "low tech or high tech class explainable information."
-
+    input_data = data.get("Age", 35)
     return jsonify({
-        "Message": message
+        "output": 345
     })
 
 @app.route("/next_weeks_program",methods=["POST"])
 def next_weeks_program():
     data = request.get_json()
     print(data)
-    input = data.get("patient baseline data", "string")
+    input = data.get("patient baseline data", "string") #excel
 
-    message = "exercise results of previous week."
+    message = "updated program." #json   {"nextExcersises": [1,2,3,4]}
 
     return jsonify({
         "Message": message
@@ -49,9 +57,9 @@ def next_weeks_program():
 def risk_of_falls1():
     data = request.get_json()
     print(data)
-    input = data.get("baseline", "string")
+    input = data.get("baseline", "string")  #excel
 
-    message = "probability."
+    message = "probability."   #double
 
     return jsonify({
         "Message": message
@@ -61,9 +69,9 @@ def risk_of_falls1():
 def risk_of_falls2():
     data = request.get_json()
     print(data)
-    input = data.get("baseline", "string")
+    input = data.get("baseline", "string")  #excel
 
-    message = "probability."
+    message = "treatment effect and side effects." #json {"level": 5}
 
     return jsonify({
         "Message": message
@@ -73,9 +81,9 @@ def risk_of_falls2():
 def risk_of_falls3():
     data = request.get_json()
     print(data)
-    input = data.get("baseline", "string")
+    input = data.get("baseline", "string")  #excel
 
-    message = "probability."
+    message = "problems side effects."   #json {"level": 5}
 
     return jsonify({
         "Message": message
