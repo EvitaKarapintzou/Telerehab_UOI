20710bc27cd97a64592ac28f753c0530fda9fbbd
Papaioannou Christoforos <xristofpap@gmail.com>
2024-03-19 10:14:02 +0200

    feat: updated the mqtt management with the mqttState (Receiving, Stop)

--- WP3/imu_mqtt/mqtt_management.py
+++ WP3/imu_mqtt/mqtt_management.py
@@ -1,14 +1,22 @@
 import time
-from shared_variables import queueData, counter, startReceiving, lastDataTime
+from shared_variables import queueData, counter, startReceiving, lastDataTime, mqttState
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe("location/123")
 
 def on_message(client, userdata, msg):
-    global counter
-    counter = counter + 1
+    global counter, mqttState 
+    counter = 1
     startReceiving.value = True   
     lastDataTime.value = time.time()
-    #print("Message Recieved. ", msg.payload.decode(), " ", counter)
+    #print("Message Received. ", msg.payload.decode(), " ", counter)
+    if 'start' in msg.payload.decode():
+        new_value = "R"
+        mqttState.value = new_value.encode()  
+        print(mqttState.value.decode()) 
+    elif 'stop' in msg.payload.decode():
+        new_value = "S"
+        mqttState.value = new_value.encode()  
+        print(mqttState.value.decode()) 
     queueData.put(msg.payload.decode())
