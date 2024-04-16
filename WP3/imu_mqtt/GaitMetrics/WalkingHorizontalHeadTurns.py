54d38b373d454c392cd3ec0b0e6e076052ecfed4
chrysa_tsimperi <tsimperi@gapps.auth.gr>
2024-04-16 15:14:20 +0300

    feat: gait events updated

--- WP3/imu_mqtt/GaitMetrics/WalkingHorizontalHeadTurns.py
+++ WP3/imu_mqtt/GaitMetrics/WalkingHorizontalHeadTurns.py
@@ -214,6 +214,9 @@ def getMetricsGaitNew02(Limu1, Limu2, plotdiagrams):
     movement_magnitude1 = np.sqrt(np.square(X_filtered1) + np.square(Y_filtered1) + np.square(Z_filtered1))
     movement_magnitude2 = np.sqrt(np.square(X_filtered2) + np.square(Y_filtered2) + np.square(Z_filtered2))
 
+    # movement_magnitude1 = np.sqrt(np.square(X_filtered1) + np.square(Z_filtered1))
+    # movement_magnitude2 = np.sqrt(np.square(X_filtered2) + np.square(Z_filtered2))
+
 
     # # Plot the combined metric over time
     # plt.figure(figsize=(12, 6))
@@ -247,7 +250,20 @@ def getMetricsGaitNew02(Limu1, Limu2, plotdiagrams):
         movement_pairs1.append((valleys1[i], peaks1[i]))
 
     print("Movement pairs for Right Leg (as index positions):", movement_pairs1)
-    
+
+    # #For evaluation for XYZ peaks and valleys
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(linear_df1.index, movement_magnitude1, label='Movement Magnitude - Right Leg', linewidth=2)
+    # plt.scatter(linear_df1.index[peaks1], movement_magnitude1[peaks1], c='red', label='Peaks', marker='x')
+    # plt.scatter(linear_df1.index[valleys1], movement_magnitude1[valleys1], c='blue', label='Valleys', marker='o')
+    # plt.xlabel('Timestamp')
+    # plt.ylabel('Magnitude of Movement')
+    # plt.title('Combined XYZ Movement Magnitude with Detected Peaks and Valleys')
+    # plt.legend()
+    # plt.show()
+    heel_strike1= len(peaks1)/4
+    toe_off1 = len(valleys1)/4
+
     #IMU2
     peaks2, _ = find_peaks(movement_magnitude2)
     valleys2, _ = find_peaks(-movement_magnitude2)
@@ -283,6 +299,9 @@ def getMetricsGaitNew02(Limu1, Limu2, plotdiagrams):
     #     plt.legend()
     #     plt.show()
 
+    heel_strike2= len(peaks2)/4
+    toe_off2 = len(valleys2)/4
+
     #=============IMU1-Right Leg====================#
     movement_ranges_yaw1 = []
     movement_ranges_pitch1 = []
@@ -316,32 +335,6 @@ def getMetricsGaitNew02(Limu1, Limu2, plotdiagrams):
 
         # Calculate durations for significant movements using timestamps
     movement_durations1 = []
-
-    # for start, end in filtered_pairs:
-    #         start_time = df_Limu1.iloc[start].name
-    #         end_time = df_Limu1.iloc[end].name
-    #         duration = (end_time - start_time).total_seconds()
-    #         movement_durations.append(duration)
-
-    # total_duration_seconds = (df_Limu1.index[-1] - df_Limu1.index[0]).total_seconds()
-    # if (total_duration_seconds > 0):
-    #         pace = len(filtered_pairs) / total_duration_seconds  
-    # else:
-    #         pace = -1;
-
-    # if (len(filtered_ranges) > 0):
-    #         mean_range = np.mean(filtered_ranges)
-    #         std_range = np.std(filtered_ranges, ddof=1) 
-    # else:
-    #         mean_range = -1;
-    #         std_range = -1;
-
-    # if (len(movement_durations) > 0):
-    #         mean_duration = np.mean(movement_durations)
-    #         std_duration = np.std(movement_durations, ddof=1)
-    #     else:
-    #         mean_duration = -1
-    #         std_duration = -1    
 
     for start, end in filtered_pairs1:
         start_time1 = df_Limu1.iloc[start].name  # Assuming the DataFrame index is datetime or similar
@@ -395,32 +388,6 @@ def getMetricsGaitNew02(Limu1, Limu2, plotdiagrams):
         # Calculate durations for significant movements using timestamps
     movement_durations2 = []
 
-    # for start, end in filtered_pairs:
-    #         start_time = df_Limu1.iloc[start].name
-    #         end_time = df_Limu1.iloc[end].name
-    #         duration = (end_time - start_time).total_seconds()
-    #         movement_durations.append(duration)
-
-    # total_duration_seconds = (df_Limu1.index[-1] - df_Limu1.index[0]).total_seconds()
-    # if (total_duration_seconds > 0):
-    #         pace = len(filtered_pairs) / total_duration_seconds  
-    # else:
-    #         pace = -1;
-
-    # if (len(filtered_ranges) > 0):
-    #         mean_range = np.mean(filtered_ranges)
-    #         std_range = np.std(filtered_ranges, ddof=1) 
-    # else:
-    #         mean_range = -1;
-    #         std_range = -1;
-
-    # if (len(movement_durations) > 0):
-    #         mean_duration = np.mean(movement_durations)
-    #         std_duration = np.std(movement_durations, ddof=1)
-    #     else:
-    #         mean_duration = -1
-    #         std_duration = -1    
-
     for start, end in filtered_pairs2:
         start_time2 = df_Limu2.iloc[start].name  # Assuming the DataFrame index is datetime or similar
         end_time2 = df_Limu2.iloc[end].name
@@ -443,7 +410,41 @@ def getMetricsGaitNew02(Limu1, Limu2, plotdiagrams):
     #print(f"Pace: {pace:.2f} movements per second")
     #print(f"Mean Movement Range: {mean_range:.2f} degrees, STD: {std_range:.2f}")
     #print(f"Mean Movement Duration: {mean_duration:.2f} seconds, STD: {std_duration:.2f}")
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    
+    heel_strikes_times_Right, properties1 = find_peaks(movement_magnitude1, prominence=0.6)  # Adjust the prominence as needed
+    print(heel_strikes_times_Right)
+    toe_off_times_Right, properties1 = find_peaks(-movement_magnitude1, prominence=0.6)  # Adjust the prominence as needed
+    print(toe_off_times_Right)
+    heel_strikes_times_Left, properties2 = find_peaks(movement_magnitude2, prominence=0.4)  # Adjust the prominence as needed
+    print(heel_strikes_times_Left)
+    toe_off_times_Left, properties2 = find_peaks(-movement_magnitude2, prominence=0.4)  # Adjust the prominence as needed
+    print(toe_off_times_Left)
+    # t1 = peaks1/100
+    # t2 = heel_strike2/100
+    # t3 = toe_off1/100
+    # t4 = toe_off2/100 
+    t1 = peaks1 / 100
+    print(t1)
+    # t2 = heel_strike2 / sampling_rate
+    t3 = valleys1 / 100  # Assuming toe_off1 is actually valleys1
+    print(t3)
+    # t4 = toe_off2 / sampling_rate
+
+    # time_vector = np.linspace(0, len(linear_df1) / 100, len(Limu1))
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(time_vector, linear_df1, label='IMU Data')
+
+    # # Add heel strikes and toe offs to the plot
+    # plt.scatter(t1, linear_df1[peaks1], color='red', label='Heel Strikes (t1)')
+    # plt.scatter(t3, linear_df1[valleys1], color='blue', label='Toe Offs (t3)')
+    # # plt.scatter(t2, Limu1[heel_strike2], color='green', label='Heel Strikes (t2)')
+    # # plt.scatter(t4, Limu1[toe_off2], color='orange', label='Toe Offs (t4)')
+
+    # plt.title('Detected Heel Strikes and Toe Offs Over Time')
+    # plt.xlabel('Time (seconds)')
+    # plt.ylabel('Sensor Data')
+    # plt.legend()
+    # plt.show()
 
     metrics_data = {
             # "movements": [
@@ -451,19 +452,29 @@ def getMetricsGaitNew02(Limu1, Limu2, plotdiagrams):
             #     for i, ((_, _), mrange, duration) in enumerate(zip(filtered_pairs, filtered_ranges, movement_durations))
             # ],
             "total_metrics": {
+                 "RIGHT IMU": {
+                "number of heelstrikes": int(heel_strike1),
+                "number of ToeOffs": int(toe_off1),
+                "Number of Steps with Right Leg": int(heel_strike1/2),
+                "Number of Steps": int(max(heel_strike1,toe_off2)),
                 "number_of_movements Right IMU": int(len(filtered_pairs1)),
                 "pace_movements_per_second Right IMU": float(pace1),
                 "mean_range_degrees Right IMU": float(mean_combined_range1),
                 "std_range_degrees Right IMU": float(std_combined_range1),
                 "mean_duration_seconds Right IMU": float(mean_duration1),
-                "std_duration_seconds Right IMU": float(std_duration1),
-
+                "std_duration_seconds Right IMU": float(std_duration1)
+                            },
+                 "LEFT IMU":{
+                "number of heelstrikes": int(heel_strike2),
+                "number of ToeOffs": int(toe_off2),
+                "Number of Steps with Left Leg": int(heel_strike2/2),
                 "number_of_movements Left IMU": int(len(filtered_pairs2)),
                 "pace_movements_per_second Left IMU": float(pace2),
                 "mean_range_degrees Left IMU": float(mean_combined_range2),
                 "std_range_degrees Left IMU": float(std_combined_range2),
                 "mean_duration_seconds Left IMU": float(mean_duration2),
                 "std_duration_seconds Left IMU": float(std_duration2)
+                            }
             }
         }
         
---
+++ WP3/plot.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+right = pd.read_csv(r'/home/uoi/Documents/GitHub/Telerehab_UOI/WP3/imu_mqtt/results/Gaitlinear/NewGait2/01_Walk_2024-03-29_14:56:45/right_FEAC84C53DE7_2024-03-29_14:56:45.csv')
+left = pd.read_csv(r'/home/uoi/Documents/GitHub/Telerehab_UOI/WP3/imu_mqtt/results/Gaitlinear/NewGait2/01_Walk_2024-03-29_14:56:45/left_C8925E7DC6BD_2024-03-29_14:56:45.csv')
+
+plt.figure(figsize=(16,12))
+
+plt.subplot(3,1,1)
+plt.plot(right['Elapsed(s)'], right['W(number)'], label = 'X Component Right', color='darkgreen')
+plt.plot(left['Elapsed(s)'], left['W(number)'], label = 'X Component Left', color='darkviolet')
+plt.title('X Value over time of 2nd New Gait' )
+plt.xlabel('Elapsed Time (s)')
+plt.ylabel('X Value')
+plt.grid(True)
+plt.legend()
+
+plt.subplot(3,1,2)
+plt.plot(right['Elapsed(s)'], right['X(number)'], label = 'Y Component Right', color='darkgreen')
+plt.plot(left['Elapsed(s)'], left['X(number)'], label = 'Y Component Left', color='darkviolet')
+plt.title('Y Value over time of 2nd New Gait' )
+plt.xlabel('Elapsed Time (s)')
+plt.ylabel('Y Value')
+plt.grid(True)
+plt.legend()
+
+plt.subplot(3,1,3)
+plt.plot(right['Elapsed(s)'], right['Y (number)'], label = 'Z Component Right', color='darkgreen')
+plt.plot(left['Elapsed(s)'], left['Y (number)'], label = 'Z Component Left', color='darkviolet')
+plt.title('Z Value over time of 2nd New Gait' )
+plt.xlabel('Elapsed Time (s)')
+plt.ylabel('Z Value')
+plt.grid(True)
+plt.legend()
+
+
+plt.tight_layout()
+plt.show()
